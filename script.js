226d1e817b7e0b35cec14b5041da21b68112422e
Derek Ngan <dd_derek129@hotmail.com>
2023-02-15 12:45:04 -0800

    added pseudocode and logical comments

---
+++ script.js
@@ -0,0 +1,24 @@
+// Pseudo code for our to do app:
+
+// 1) save the values / DOM elements we will need to interact with
+// let's query the DOM for the form element and save it within a variable
+
+// let's query the DOM for the input element and save it within a variable
+
+// 2) attach an event listener to the form element
+// pass in 2 arguments to the event listener method:
+// a) the event you are listening for
+// b) the callback function which will hold the logic we wish to run ONCE the event "is heard" (AKA occurs on the page)
+
+// save the text value from the input within a variable
+
+// IF the user submits a todo (check whether the value of the input it NOT empty) then:
+// create a li element
+// add a FA icon to the element
+// add the text from the todo that the user entered to the li
+
+// append the li element to the ul element
+// ELSE alert user to please submit a valid to do 
+
+
+// 3) KENZIE'S STEP: how do we track when the task is done?
